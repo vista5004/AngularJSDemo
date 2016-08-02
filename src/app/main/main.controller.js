e8995bfd695ee64cacade3818b0bacc673acf2fb
cycle <vista5004@gmail.com>
2016-08-03 00:40:08 +0800

    ui

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -3,14 +3,29 @@
 
   angular
     .module('angularJsdemo')
-    .controller('MainController', ['$scope', function ($scope) {
+    .controller('MainController', ['$scope','$rootScope',function ($scope,$rootScope) {
         var vm=$scope.vm={};
         vm.superName="Cycle";
         vm.tittle="管理员";
         vm.isDivContent=false;
         vm.openContent= function () {
           vm.isDivContent=!vm.isDivContent;
-        }
+        };
+       /* $rootScope.$on("$stateChangeSuccess", function () {
+          var width=$("window").width();
+          var contentWidth=width-220;
+          $(".mainPage").css({
+            "width":contentWidth
+          })
+        })*/
+      $scope.$on('$viewContentLoaded', function () {
+        var width=$(window).width();
+        var contentWidth=width-220;
+        console.log(contentWidth);
+        $(".mainPage").css({
+          "width":contentWidth
+        })
+      })
     }]);
 
 
