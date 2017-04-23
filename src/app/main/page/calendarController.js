f7c94a879af860f9fc46cacbc4eea89b4af538cc
cycle <vista5004@gmail.com>
2017-04-23 23:03:19 +0800

    calendar

--- src/app/main/page/calendarController.js
+++ src/app/main/page/calendarController.js
@@ -5,16 +5,26 @@
   angular
     .module("angularJsdemo")
     .controller("calendarController", ['$scope', function ($scope) {
-      /*var date=$scope.search.Date;
-      console.log($scope.search.Date);
-      /!*设置*!/
+      var date=$scope.searchDate;
+      console.log(date);
       $scope.today= function () {
-
         $scope.dt=new Date();
-
-      };*/
-
-
-
+      };
+      $scope.today();
+      /*基础设置*/
+      $scope.options={
+        minDate: new Date(),
+        showWeeks: true
+      };
+      $scope.setData= function (year, month, day) {
+        $scope.dt=new Date(year,month,day)
+      };
+      $scope.clear= function () {
+        $scope.dt=null;
+      };
+      $scope.toggleMin= function () {
+        $scope.options.minDate = $scope.options.minDate ? null : new Date();
+      };
+      $scope.toggleMin();
     }])
 })();
